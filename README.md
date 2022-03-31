aa15be8aec857a969a0490ef394a90d94036c4ef
Lepus <lepus@lepus.site>
2022-03-31 19:59:20 +1000

    Roughly Works internally,
    Not working yet, have to write Opt accessor logic.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # bOpts
  Command Line Options Handling
+
+ ### By Lepus Hare.
+
+ Parses command line options.
+
+Usage:
+
+    SetOpts(flag, variant[])//Allows for the defining of collapsing multiple opts into 1 opt.
+                            // ie: SetOpts("f", {"fix", "fixes", "fi"})
+                            // will collapse -f, -fix -fixes -fi into 1 value;
+
+    MakeOpts(argc, argv);   //Allows for the usage of the following commands.
+
+    //All GetOpt variants return a std::string.
+    GetOpt();               //Returns argv as a std::string;
+    GetOpt("-F");           //Returns a pointer to the string with the -F switch
+    GetOpt(n, head=true);   //For a positive integer n and an optional bool 'head':
+                            // if head  : return argv[n-1]
+                            // else     : return argv[argc-n-1]
+                            // ie: GetOpt(0, false) returns the last option. 
+    GetFloatingOpt(n);      //Returns the nth opt that is not preceeded directly by a switch.
+    GetSwitchOpt(switch);   //Returns the opt following switch.
+
+    FreeOpts();             //Frees any Opt memory, will make Opt commands no longer work.
+
+Logic:
+    Loops argv[] and populates a std::map<string>
+
+
+
+ Todo:
+    Everything.
---
+++ bOpts.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <map>
+#include <typeinfo>
+
+// 
+// bOpts: Bunny command parsing.
+//      By L3pu5, L3pu5_Hare, Lepus Hare.
+
+int bOpts_FLOATINGCOUNT = 0;
+std::map <std::string, std::string> bOpts_MAP = std::map<std::string, std::string>();
+
+void populate(int argc, char* _argv[]){
+    //Internal variables
+    bool _priorSwitch = 0;
+    std::string _key = "";
+    //Main Loop.
+    for (int i = 1; i < argc; i++)
+    {
+        //Read the string
+        std::string _argument(_argv[i]);
+        //Check if prior was a switch
+        if(_priorSwitch == 0){
+            //If not, is this a switch?
+            if(_argument.at(0) == '-'){
+                _key = _argument.substr(1);
+                _priorSwitch = 1;
+                continue;
+            }
+            _key += "Floating_";
+            _key += std::to_string(bOpts_FLOATINGCOUNT);
+            //If this is a value and not a switch, feed it into the map as one of the floating values.
+            bOpts_MAP.insert({_key, _argument});
+            bOpts_FLOATINGCOUNT++;
+            _key = "";
+            continue;
+        }
+        //Prior was a switch
+        //Read it directly
+        bOpts_MAP[_key] = _argument;
+        _key = "";
+        _priorSwitch = 0;
+        continue;
+    }
+
+    
+}
+
+int main(int argc, char * argv[]){
+    populate(argc, argv);
+}
